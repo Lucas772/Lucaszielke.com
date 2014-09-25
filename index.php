95ea0647997e368251256580ba89aa4833bc1361
lucas772 <lucas.zielke1@gmail.com>
2014-09-25 17:45:03 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,41 @@
+<html>
+	<?php include 'basicinfo.php';?>
+	<link rel="stylesheet" type="text/css" href="stylesheets/styles.css">
+
+	<div id="Content">
+  		<div id="Header"> <!-- Header -->
+  			<h6>Lucas Zielke | </h6><h4>A world of Lucas's</h4>
+  		</div>
+
+
+  		<div id="Navigation"> <!--Navigation menu -->
+  			<div class="Page1">
+  				<a href"index.php">Home<img src=""></a>
+  			</div>
+ 			<div class="Page2">
+  				<a href"Page2.php"><?php $page2_title?></a>
+  			</div>
+ 			<div class="Page3">
+  				<a href"Page3.php"><?php $page3_title?></a>
+  			</div>
+ 			<div class="Login">
+  				<a href"LogIn.php">Log In</a>
+  			</div>
+  		</div>
+
+
+
+  		<div id="Content-wrapper"> <!-- Specific pages content in a whole -->
+  			<div id="Content"><!-- The Actual main content -->
+
+  			</div>
+  			<div id="Content-Sidebar"><!-- Sidebar of the content -->
+
+		  	</div>
+  		</div>
+
+
+
+  		<?php include 'footer.php';?>
+	</div>
+</html>
